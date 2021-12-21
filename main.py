327664d3e317faa1ffe228eda0cdcf3a6e9ca16f
ballban <ballban@hotmail.com>
2021-12-22 03:40:47 +0900

    commit

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from discord.ext import commands
 import os
 import azure.cognitiveservices.speech as speechsdk
 from azure.cognitiveservices.speech import AudioDataStream, SpeechSynthesisOutputFormat
-import voice_data
+import voice_data as vd
 import re
-
-SINGLE_COMMAND_LIST = ['update_voice_list', 'leave', 'l', 'help', 'h']
-COMMAND_LIST = ['set_voice', 'set_default_voice']
 
 # Azure_TTS
 # Creates an instance of a speech config with specified subscription key and service region.
@@ -17,23 +14,82 @@ speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_r
 
 # discord
 bot = discord.Client()
-bot = commands.Bot(command_prefix='`')
+bot = commands.Bot(command_prefix='!')
+
+# get voice data
+voice_module = vd.VoiceModule()
+voice_list = vd.get_voice_list_from_local()
+
+# user_voice_data = voice_module.get_user_data("default")
+# print(user_voice_data.voice_setting)
+# # user_voice_data.voice_setting["en"] = voice_module.search("en-US-JennyNeural").pop()
+# # user_voice_data.voice_setting["jp"] = voice_module.search("ja-JP-KeitaNeural").pop()
+# # user_voice_data.voice_setting["kr"] = voice_module.search("ko-KR-SunHiNeural").pop()
+# # user_voice_data.voice_setting["tw"] = voice_module.search("zh-TW-HsiaoChenNeural").pop()
+# # user_voice_data.voice_setting["hk"] = voice_module.search("zh-HK-WanLungNeural").pop()
+# user_voice_data.voice_setting["default"] = voice_module.search("zh-CN-XiaoxiaoNeural").pop()
+# print(user_voice_data.voice_setting)
+# voice_module.save_user_data(user_voice_data)
+
+# for x in voice_module.search("chinese"):
+#     print(x.short_name)
 
 
 @bot.command()
-async def set_voice(ctx, voice_name):
-    await ctx.send(f"set_voice: {voice_name}")
+async def h(ctx):
+    default_voice_data = voice_module.get_user_data("default")
+    default_voice_message = "\nHere is default voice setting:"
+    for (key, value) in default_voice_data.voice_setting.items():
+        default_voice_message += f"\n       {key} : {value.short_name}"
+
+    string = "How to use this bot:" \
+             "\n        Type __`你好__ or __'en Hello__" \
+             "\n        Use symbol __`(language key) (text)__" \
+             + default_voice_message + \
+             "\n" \
+             "\nYou can setup your own voice setting." \
+             "\nUse command __!set_voice (custom_key) (voice_name)__" \
+             "\n                        __!set_default_voice (voice_name)__" \
+             "\nAnd __!search (key1) (key2)__ to search usable voice name" \
+             "\nExample:" \
+             "\n        First, search for a voice __!search korean__" \
+             "\n        Then, !set_voice ko ko-KR-SunHiNeural" \
+             "\n                !set_default_voice en-GB-LibbyNeural" \
+             "\n        And you can use those voices by tying __`ko 안녕__ or __`hello__" \
+             "\nVisit here to hear sample audio:" \
+             "\nhttps://azure.microsoft.com/en-us/services/cognitive-services/text-to-speech/#features" \
+             "\n" \
+             "\ntype __!command__ for more command information"
+    await ctx.send(string)
 
 
 @bot.command()
-async def set_default_voice(ctx, voice_name):
-    await ctx.send(f"set_default_voice: {voice_name}")
-
-
-@bot.command()
-async def update_voice_list(ctx):
-    voice_data.get_voice_list_from_microsoft()
-    await ctx.send("Done!")
+async def command(ctx):
+    string = "\nCommand list:" \
+             "\n`!leave`" \
+             "\nLet bot leave the voice channel" \
+             "\n" \
+             "\n`!update_voice_list`" \
+             "\nUpdate voice list from microsoft official website" \
+             "\n" \
+             "\n`!show_voice_setting`" \
+             "\nShows a list of your voice custom setting" \
+             "\n" \
+             "\n`!delete_profile`" \
+             "\nDelete your profile" \
+             "\n" \
+             "\n`!delete_voice_setting (key)`" \
+             "\nDelete your voice setting with key" \
+             "\n" \
+             "\n`!set_voice (custom_key) (voice_name)`" \
+             "\nSet voice with specific key" \
+             "\n" \
+             "\n`!set_default_voice (voice_name)`" \
+             "\nWhen u didn't define the language, this voice will be chosen" \
+             "\n" \
+             "\n`!search (key1) (key2)`" \
+             "\nSearch for a voice"
+    await ctx.send(string)
 
 
 @bot.command()
@@ -53,6 +109,104 @@ async def leave(ctx):
         await ctx.send("Not in a voice channel")
 
 
+@bot.command()
+async def update_voice_list(ctx):
+    voice_list = vd.get_voice_list_from_microsoft()
+    await ctx.send("Done!")
+
+
+@bot.command()
+async def set_voice(ctx, key, voice_name):
+    # Replace invalid symbol
+    key = re.sub('[<>/|":*]', '', key)
+    key = key.replace("\\", " ")
+
+    # Get user voice data
+    user_voice_data = voice_module.get_user_data(str(ctx.author.id), False)
+    if not user_voice_data.user_id:
+        user_voice_data.user_id = str(ctx.author.id)
+        user_voice_data.user_name = ctx.author.name
+
+    # Search for voice data
+    voice_search_data = voice_module.search(voice_name)
+    if len(voice_search_data) == 1:
+        # Set voice
+        user_voice_data.voice_setting[key] = voice_module.search(voice_name).pop()
+        # Save voice data
+        voice_module.save_user_data(user_voice_data)
+        await ctx.send(f"set_voice: {voice_name}")
+    else:
+        await ctx.send(f"Wrong voice name! : {voice_name}\n"
+                       f"Use !search or visit\n"
+                       f"https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#prebuilt-neural-voices"
+                       f"to get correct voice name!")
+
+
+@bot.command()
+async def set_default_voice(ctx, voice_name):
+    await set_voice(ctx, "default", voice_name)
+
+
+@bot.command()
+async def search(ctx, search_key1: str, search_key2=""):
+    voice_data_search_list = voice_module.search(search_key1, search_key2)
+
+    result = ""
+    for voice_data in voice_data_search_list:
+        result += f"{voice_data.short_name} {voice_data.gender} {voice_data.locale_name}\n"
+
+    if voice_data_search_list:
+        if len(result) > 2000:
+            result = result[:1950] + "........\n........."
+        await ctx.send(f"Search result:\n{result}")
+        # else:
+        #     await ctx.send(f"Too many results.\n"
+        #                    f"Please change search key and try again!")
+    else:
+        await ctx.send(f"No data found")
+
+
+@bot.command()
+async def show_voice_setting(ctx):
+    # Get user voice data
+    user_voice_data = voice_module.get_user_data(str(ctx.author.id), False)
+    if not user_voice_data.user_id:
+        await ctx.send(f"User profile doesn't exist!")
+    else:
+        result = "Here is your voice setting\n" \
+                 "---------------------------------\n"
+        for (key, value) in user_voice_data.voice_setting.items():
+            result += f"{key} : {value.short_name}\n"
+        result += "---------------------------------\n"
+        await ctx.send(f"{result}")
+
+
+@bot.command()
+async def delete_voice_setting(ctx, key):
+    # Get user voice data
+    user_voice_data = voice_module.get_user_data(str(ctx.author.id), False)
+    if not user_voice_data.user_id:
+        await ctx.send(f"User profile doesn't exist!")
+    else:
+        if key in user_voice_data.voice_setting:
+            user_voice_data.voice_setting.pop(key)
+            voice_module.save_user_data(user_voice_data)
+            await ctx.send(f"Setting {key} deleted!")
+        else:
+            await ctx.send(f"Wrong key!\nPlease check your voice setting by using !show_voice_setting")
+
+
+@bot.command()
+async def delete_profile(ctx):
+    # Get user voice data
+    user_voice_data = voice_module.get_user_data(str(ctx.author.id), False)
+    if not user_voice_data.user_id:
+        await ctx.send(f"User profile doesn't exist!")
+    else:
+        voice_module.delete_user_data(user_voice_data)
+        await ctx.send(f"Your profile has been deleted!")
+
+
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
@@ -64,114 +218,108 @@ async def on_message(message: discord.Message):
     if message.author == bot.user:
         return
 
-    if (message.content.startswith(f"`") or message.content.startswith(f"｀")) and len(message.content) > 1:
+    if message.content.startswith("!"):
+        await bot.process_commands(message)
+        return
+
+    if (message.content.startswith("`") or message.content.startswith("｀")) and len(message.content) > 1:
         # Synthesizes the received text to speech.
         # The synthesized speech is expected to be heard on the speaker with this line executed.
 
-        # Check command
-        temp_list = message.content[1:].split()
-        if temp_list[0] in COMMAND_LIST:
-            await bot.process_commands(message)
+        # Check bot voice channel
+        bot_voice_client = await join(message)
+        if not bot_voice_client:
             return
 
-        # Check bot voice channel
-        bot_voice_client = next(
-            (voice_client for voice_client in bot.voice_clients
-             if voice_client.guild == message.author.guild), None)
-        if await join(message, bot_voice_client):
-            bot_voice_client = next(
-                (voice_client for voice_client in bot.voice_clients
-                 if voice_client.guild == message.author.guild), None)
+        # Replace invalid symbol
+        text = re.sub('[<>/|":*]', ' ', message.content[1:])
+        text = text.replace("\\", " ")
 
-            # Replace invalid symbol
-            text = re.sub('[<>/|":*]', ' ', message.content[1:])
-            text = text.replace("\\", " ")
+        if len(text) > 0:
+            # Get user voice data
+            user_voice_data = voice_module.get_user_data(str(message.author.id))
+            default_voice_data = voice_module.get_user_data("default")
 
-            if len(text) > 0:
-                if text.startswith('en'):
-                    language = 'en-US'
-                    voice_name = 'en-US-JennyNeural'
-                    text = ' '.join(text.split()[1:])
-                elif text.startswith('jp'):
-                    language = 'ja-JP'
-                    voice_name = 'ja-JP-KeitaNeural'
-                    text = ' '.join(text.split()[1:])
-                elif text.startswith('kr'):
-                    language = 'ja-JP'
-                    voice_name = 'ko-KR-SunHiNeural'
-                    text = ' '.join(text.split()[1:])
-                elif text.startswith("hk"):
-                    language = "zh-HK"
-                    voice_name = 'zh-HK-WanLungNeural'
-                    text = ' '.join(text.split()[1:])
-                elif text.startswith('tw'):
-                    language = 'zh-TW'
-                    voice_name = 'zh-TW-HsiaoChenNeural'
-                    text = ' '.join(text.split()[1:])
-                else:
-                    # text.startswith("chinese") or text.startswith("cn"):
-                    language = 'zh-CN'
-                    voice_name = 'zh-CN-XiaoxiaoNeural'
+            # Check language key
+            text_split_list = text.split()
+            # Set language key
+            if len(text_split_list) > 1:
+                text_language_key = text.split()[0]
+                text = " ".join(text.split()[1:])
+            else:
+                text_language_key = "default"
+            # Set language and voice_name
+            if text_language_key in user_voice_data.voice_setting:
+                language = user_voice_data.voice_setting[text_language_key].locale
+                voice_name = user_voice_data.voice_setting[text_language_key].short_name
+            else:
+                language = default_voice_data.voice_setting[text_language_key].locale
+                voice_name = default_voice_data.voice_setting[text_language_key].short_name
 
-                audio_file_path = f"AudioFile/{voice_name}/{text}.ogg"
-                if text == "test_music":
-                    audio_file_path = "AudioFile/1.m4a"
+            # Create audio file path
+            audio_file_path = f"AudioFile/{voice_name}/{text}.ogg"
+            if text == "test_music":
+                audio_file_path = "AudioFile/1.m4a"
 
-                # if file doesn't exist, request for it
-                if not os.path.exists(audio_file_path):
-                    speech_config.speech_synthesis_language = language
-                    speech_config.speech_synthesis_voice_name = voice_name
-                    speech_config.set_speech_synthesis_output_format(
-                        SpeechSynthesisOutputFormat["Ogg16Khz16BitMonoOpus"])
+            # if file doesn't exist, request for it
+            if not os.path.exists(audio_file_path):
+                speech_config.speech_synthesis_language = language
+                speech_config.speech_synthesis_voice_name = voice_name
+                speech_config.set_speech_synthesis_output_format(
+                    SpeechSynthesisOutputFormat["Ogg16Khz16BitMonoOpus"])
 
-                    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+                speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
 
-                    result = speech_synthesizer.speak_text_async(text).get()
+                result = speech_synthesizer.speak_text_async(text).get()
 
-                    # Checks result.
-                    if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-                        print("Speech synthesized to speaker for text [{}]".format(text))
-                    elif result.reason == speechsdk.ResultReason.Canceled:
-                        cancellation_details = result.cancellation_details
-                        print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-                        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-                            if cancellation_details.error_details:
-                                print("Error details: {}".format(cancellation_details.error_details))
-                        print("Did you update the subscription info?")
+                # Checks result.
+                if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+                    print("Speech synthesized to speaker for text [{}]".format(text))
+                elif result.reason == speechsdk.ResultReason.Canceled:
+                    cancellation_details = result.cancellation_details
+                    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+                    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                        if cancellation_details.error_details:
+                            print("Error details: {}".format(cancellation_details.error_details))
+                    print("Did you update the subscription info?")
 
-                    # Change <?> to ASCII before save
-                    audio_file_path = audio_file_path.replace('?', '&#63;')
-
-                    # Save file to local
-                    stream = AudioDataStream(result)
-                    # Check folder path
-                    audio_folder_path = os.path.dirname(audio_file_path)
-                    if not os.path.exists(audio_folder_path):
-                        # Create a new directory
-                        os.makedirs(audio_folder_path)
-                    stream.save_to_wav_file(audio_file_path)
-
-                # Change <?> to ASCII before load
+                # Change <?> to ASCII before save
                 audio_file_path = audio_file_path.replace('?', '&#63;')
 
-                audio_source = discord.FFmpegOpusAudio(source=audio_file_path)
-                # audio_source = discord.FFmpegPCMAudio(source=audio_file_path)
+                # Save file to local
+                stream = AudioDataStream(result)
+                # Check folder path
+                audio_folder_path = os.path.dirname(audio_file_path)
+                if not os.path.exists(audio_folder_path):
+                    # Create a new directory
+                    os.makedirs(audio_folder_path)
+                stream.save_to_wav_file(audio_file_path)
 
-                bot_voice_client.play(audio_source)
+            # Change <?> to ASCII before load
+            audio_file_path = audio_file_path.replace('?', '&#63;')
 
-                # Send discord message
-                # await message.channel.send(f"{text}")
+            audio_source = discord.FFmpegOpusAudio(source=audio_file_path)
+            # audio_source = discord.FFmpegPCMAudio(source=audio_file_path)
+
+            while bot_voice_client.is_playing():
+                bot.sleep(.1)
+            bot_voice_client.play(audio_source)
+
+            # Send discord message
+            # await message.channel.send(f"{text}")
 
 
-async def join(ctx, bot_voice_client):
+async def join(ctx):
     """
     Connect/move to voice channel where user is inside
     :param ctx: I have no idea what is this
-    :param bot_voice_client: bot voice client
-    :return: bool
+    :return: bot_voice_client
     """
-    join_check = True
+    # If user in voice channel
     if ctx.author.voice:
+        # Get current bot voice channel
+        bot_voice_client = next((voice_client for voice_client in bot.voice_clients
+                                 if voice_client.guild == ctx.author.guild), None)
         user_voice = ctx.author.voice
         # If bot in a voice channel
         if bot_voice_client:
@@ -180,11 +328,11 @@ async def join(ctx, bot_voice_client):
                 await bot_voice_client.move_to(user_voice.channel)
         # Or join the author channel
         else:
-            await user_voice.channel.connect()
+            bot_voice_client = await user_voice.channel.connect()
     else:
         await ctx.channel.send("You are not in a voice Channel!")
-        join_check = False
-    return join_check
+        bot_voice_client = None
+    return bot_voice_client
 
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
--- voice_data.py
+++ voice_data.py
@@ -2,6 +2,72 @@ import requests
 import json
 import os
 
+VOICE_LIST_DATA_PATH = "Data/voice_list_data.json"
+USER_DATA_PATH = "Data/user_data.json"
+
+
+class VoiceModel:
+    """
+    Voice model for voice list
+    """
+    def __init__(self, voice: dict):
+        self.data_raw = voice
+        self.name = voice["Name"]
+        self.display_name = voice["DisplayName"]
+        self.local_name = voice["LocalName"]
+        self.short_name = voice["ShortName"]
+        self.gender = voice["Gender"]
+        self.locale = voice["Locale"]
+        self.locale_name = voice["LocaleName"]
+        self.sample_rate_hertz = voice["SampleRateHertz"]
+        self.voice_type = voice["VoiceType"]
+        self.status = voice["Status"]
+        self.style_list = voice["StyleList"] if "StyleList" in voice else None
+        self.role_play_list = voice[
+            "RolePlayList"] if "RolePlayList" in voice else None
+
+    def to_json(self):
+        dic = {"Name": self.name,
+               "DisplayName": self.display_name,
+               "LocalName": self.local_name,
+               "ShortName": self.short_name,
+               "Gender": self.gender,
+               "Locale": self.locale,
+               "LocaleName": self.locale_name,
+               "SampleRateHertz": self.sample_rate_hertz,
+               "VoiceType": self.voice_type,
+               "Status": self.status}
+        if self.style_list:
+            dic["StyleList"] = self.style_list
+        if self.role_play_list:
+            dic["RolePlayList"] = self.role_play_list
+        return dic
+
+
+class UserModel:
+    """
+    Model for user
+    """
+    def __init__(self, user_data):
+        if user_data:
+            self.data_raw = user_data
+            self.user_id = user_data["UserId"]
+            self.user_name = user_data["UserName"] if "UserName" in user_data else None
+            self.voice_setting = {key: VoiceModel(value) for (key, value) in user_data["VoiceSetting"].items()}
+        else:
+            self.data_raw = None
+            self.user_id = None
+            self.user_name = None
+            self.voice_setting = {}
+
+    def to_json(self):
+        dic = {
+            "UserId": self.user_id,
+            "UserName": self.user_name,
+            "VoiceSetting": {key: value.to_json() for (key, value) in self.voice_setting.items()}
+        }
+        return dic
+
 
 def __get_access_token(subscription_key):
     """
@@ -10,9 +76,7 @@ def __get_access_token(subscription_key):
     :return: access_token
     """
     fetch_token_url = 'https://japaneast.api.cognitive.microsoft.com/sts/v1.0/issueToken'
-    headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key
-    }
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
     response = requests.post(fetch_token_url, headers=headers)
     access_token = str(response.text)
     return access_token
@@ -36,34 +100,102 @@ def __get_voice_list(subscription_key, access_token):
     return voice_list
 
 
+def get_user_data_list():
+    """
+    Get user data list from USER_DATA_PATH
+    :return: User data list
+    """
+    with open(USER_DATA_PATH, "r") as file:
+        user_data_list = json.load(file)
+    return user_data_list
+
+
+def get_voice_list_from_local():
+    """
+    Get voice list from voice_list_data.json
+    :return: voice_list
+    """
+    with open(VOICE_LIST_DATA_PATH, "r") as file:
+        voice_list = json.load(file)
+    return voice_list
+
+
 def get_voice_list_from_microsoft():
     """
     Get voice list and save file to voice_list_data.json
-    :return: nothing
+    :return: voice_list
     """
     subscription_key = os.environ['AZURE_TTS_TOKEN']
     access_token = __get_access_token(subscription_key)
     voice_list = __get_voice_list(subscription_key, access_token)
     if voice_list:
-        with open("Data/voice_list_data.json", "w") as file:
+        with open(VOICE_LIST_DATA_PATH, "w") as file:
             json.dump(voice_list, file, indent=4)
+        return voice_list
     else:
         print(f"Something wrong with voice_list: {voice_list}")
 
 
-class VoiceModel:
-    """
-    Voice model for voice list
-    """
-    def __init__(self, voice: dict):
-        self.name = voice["Name"]
-        self.displayname = voice["DisplayName"]
-
-
-
-class UserVoiceModel:
-    """
-    Voice model for user
-    """
+class VoiceModule:
     def __init__(self):
-        self.default_voice = None
+        self.voice_list = get_voice_list_from_local()
+        self.user_data_list = get_user_data_list()
+
+    def save_user_data(self, user_model: UserModel):
+        """
+        Save user data setting to local file
+        :param user_model: User model
+        :return: nothing
+        """
+        self.user_data_list[str(user_model.user_id)].update(user_model.to_json())
+        with open(USER_DATA_PATH, "w") as file:
+            json.dump(self.user_data_list, file, indent=4)
+
+    def delete_user_data(self, user_model: UserModel):
+        """
+        Delete user data setting
+        :param user_model: User model
+        :return: nothing
+        """
+        self.user_data_list.pop(str(user_model.user_id))
+        with open(USER_DATA_PATH, "w") as file:
+            json.dump(self.user_data_list, file, indent=4)
+
+    def get_user_data(self, user_id: str, default=True):
+        """
+        Get user data by user id
+        :param user_id: User id from discord
+        :param default: Get default user if user id doesn't exist
+        :return: UserModel or None
+        """
+        if user_id in self.user_data_list:
+            user_data = self.user_data_list[user_id]
+        elif default:
+            user_data = self.user_data_list["default"]
+        else:
+            user_data = None
+        return UserModel(user_data)
+
+    def search(self, search_key1: str, search_key2=""):
+        """
+        Search and return every voice data which contains search_key1 and search_key2
+        :param search_key1: search_key1
+        :param search_key2: search_key2
+        :return: VoiceModel list
+        """
+        result = [
+            VoiceModel(voice) for voice in self.voice_list if len({
+                value
+                for (key, value) in voice.items()
+                if type(value) == str and value.lower().find(search_key1.lower()) >= 0
+            }) > 0
+        ]
+        if search_key2:
+            result = [
+                x for x in result if len({
+                    value
+                    for (key, value) in x.data_raw.items()
+                    if type(value) == str and value.lower().find(search_key2.lower()) >= 0
+                }) > 0
+            ]
+        return result
